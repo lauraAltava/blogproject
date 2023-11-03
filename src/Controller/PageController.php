d059c2caf5b8cfa59626209d5c1cf59969daf47c
lauraAltava <laumaltava@gmail.com>
2023-11-03 10:16:22 +0100

    falta usuarios

---
+++ src/Controller/PageController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('page/about.html.twig', []);
+    }
+
+    #[Route('/service', name: 'service')]
+    public function service(): Response
+    {
+        return $this->render('page/service.html.twig', []);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('page/contact.html.twig', []);
+    }
+
+    #[Route('/team', name: 'team')]
+    public function team(): Response
+    {
+        return $this->render('page/team.html.twig', []);
+    }
+
+}
